3088880c10afae673fa94567f2cf7beb4cdc378a
Grigoriyy0 <padgamerrfp@gmail.com>
2023-08-15 19:08:27 +0400

    urls

--- webblog/blog/models.py
+++ webblog/blog/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    '''данные о посте'''
     title = models.CharField('Заголовок записи', max_length=100)
     description = models.TextField('Текст записи')
-    author = models.CharField('Имя автора', max_lenght=100)
+    author = models.CharField('Имя автора', max_length=100)
     date = models.DateField('Дата публикации')
 
     class Meta:
